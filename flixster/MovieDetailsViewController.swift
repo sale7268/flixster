4bddd010717c4a4ef53f010bcc0dd1026576b9b0
Hamza Saleem <hsaleem@csumb.edu>
2021-02-15 21:51:00 -0800

    Completed unit 2 required stories

---
+++ flixster/MovieDetailsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MovieDetailsViewController.swift
+//  flixster
+//
+//  Created by user190273 on 2/15/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieDetailsViewController: UIViewController {
+
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    var movie: [String:Any]!
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Drawing code
+
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        posterView.af.setImage(withURL: posterUrl!)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+        backdropView.af.setImage(withURL: backdropUrl!)
+        
+    }
+}
---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by user190273 on 2/15/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
